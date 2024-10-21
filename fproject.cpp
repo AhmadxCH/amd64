c83fadd286b38f892f9a22dd4ce7279ee32f9503
AhmadxCH <imahmad1414@gmail.com>
2024-10-21 13:42:45 +0500

    Add files via upload

---
+++ fproject.cpp
@@ -0,0 +1,72 @@
+
+
+
+// Section A2
+//Lab2
+//task 1 
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+//{/*
+//	cout << "*************
+//	cout << "Hello world" << endl;"<<endl;
+//	cout << "*************"<<endl; 
+//cout << "*************" << endl;
+//cout << "*           *" << endl;
+//cout << "*************" << endl;
+//return 0;*/
+//}
+  //task 3 
+  //{
+	 // cout << "        *" << endl;
+	 // cout << "       * *" << endl;
+	 // cout << "      *   *" << endl;
+	 // cout << "     *******" << endl;
+	 // return 0;
+ // //}
+	//task 4
+/*
+    cout << "        *" << endl;
+    cout << "       ***" << endl;
+    cout << "      *****" << endl;
+    cout << "     *******" << endl;
+    return 0;
+}*/
+//task 5
+//{
+//    cout << "       ****" << endl;
+//    cout << "      *   *" << endl;
+//    cout << "      *   *" << endl;
+//    cout << "      ****" << endl;
+//    return 0;
+//}
+//task 6
+//{ 
+//    cout << "  ******** " << endl;
+//    cout << "  *      *" << endl;
+//    cout << "  *      *" << endl;
+//    cout << "  ********" << endl;
+//    return 0;
+//}
+//task 7
+//{
+//	   cout << "    *" << endl;
+//	   cout << "  *   *" << endl;
+//	   cout << " *     *" << endl;
+//       cout << "  *   * " << endl;
+//	   cout << "    *   " << endl;
+//	   return 0;
+//	}
+//task 8 
+{ /*
+   cout << "        *" << endl;
+   cout << "      ***** " << endl;
+   cout << "     *******" << endl;
+   cout << "      *****" << endl;
+   cout << "        *  " << endl;
+   return 0;
+}*/
